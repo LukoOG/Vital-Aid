6572c8cb01ce153c16a886a1352a4514eab0aec6
unknown <emmanueladesipe01@gmail.com>
2025-02-22 18:33:19 +0100

    added AI response

--- vital-aids/src/lib/client/aiResponse.ts
+++ vital-aids/src/lib/client/aiResponse.ts
@@ -1,10 +1,12 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from "dotenv"
+import { Message } from "../types";
 
 dotenv.config()
 
 let api_key: string = process.env.API_KEY || ""
 const genAI = new GoogleGenerativeAI(api_key)
+const GEMINI_MODEL = "gemini-1.5-flash";
 
 const generationConfig = {
   temperature: 1,
@@ -14,3 +16,33 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
+export async function AiChatResponse(
+    history: Message[],
+    prompt: string,
+    systemPrompt:string
+){
+    const model = genAI.getGenerativeModel({
+        model: GEMINI_MODEL,
+        systemInstruction: systemPrompt,
+        generationConfig,
+    });
+    const chatSession = model.startChat({ history: history });
+    const result = await chatSession.sendMessage(prompt);
+    const text = result.response.text();
+    // console.log(text);
+    return text;
+}
+
+export async function AiAidResponse(prompt:string, systemPrompt:string){
+    const model = genAI.getGenerativeModel({
+        model: GEMINI_MODEL,
+        systemInstruction: systemPrompt,
+        generationConfig,
+});
+
+const result = await model.generateContent(prompt);
+const text = result.response.text();
+
+console.log(text);
+return text;
+}
---
+++ vital-aids/src/lib/client/prompt.ts
@@ -0,0 +1,25 @@
+export const firstAidSysPrompt = `
+As an Ai health emergency agent, Your goals is to guide someone in an emergency situation on how to handle a given emergency situation. 
+For example, where someone has been badly cut, injured in a car crash, or has fallen from a scaffolding at work
+Knowing how to remain calm, work around barriers, and to provide the appropriate response to an injured person can mean the difference between life and death.
+Know that the country the person rersides does not have 911, so guide them as there is no immediate help around. Make sure to question them 1 at a time to get further information then give a concise response as to how they can handle the situation. 
+Keep your response as concise and effective to the situation as possible.
+In case of a sever situation where professional medical assistance is needed, refer them to the /hospital page of the webapp.`;
+
+export const hospitalSysPrompt = `You are an ai agent designed to be used for health emergency situation and you return only json as output. This is the full list of relevant emergency treatments and specialities hospitals,clinic and primary health center can offer.
+  TREATMENTS
+Acute Renal Failure, Cardiac Arrest, Heart Attack, Stroke, Pulmonary Embolism, Pneumonia, Tuberculosis, CT Scan, MRI, Ultrasound, X-ray, Beating Heart Surgery, Bentall Procedure, Brain & Spinal Cord Cancer, Breast Cancer, Cardiovascular Surgery, Coronary Artery Bypass Grafting, Epilepsy Surgery, Heart Transplant, Heart Valve Surgery (Repair, Replacement), Kidney Transplant, Liver Transplant, Lung Cancer, Paediatric Cardiac Surgery, Open Heart Surgery, Stentless Heart Valve Surgeries, Trauma Care, Normal & Instrumental Delivery, High Risk Pregnancy, Uterine Cancer, Ovarian Cancer
+SPECIALITIES
+-----------
+Arthroscopy & Sports Medicine, Cardiac Surgery, Cardiology, Critical Care Medicine, Emergency Medicine, ENT, Gastroenterology Surgical, Gastroenterology Medical, General Medicine/Internal Medicine, General Surgery, Haematology, Interventional Radiology, Lab Medicine, Medical Oncology, Minimal Access Surgery, Neonatology, Nephrology, Neurology, Neurosurgery, Orthopaedics, Paediatric Cardiac Surgery, Paediatric Cardiology, Paediatric Neurology, Paediatric Neurosurgery, Paediatric Surgery, Paediatric Urology, Paediatrics, Pulmonology, Radiology, Surgical Oncology, Urology, Vascular & Endovascular Surgery, Vascular & Interventional Radiology, Vascular Surgery
+Command: You will be provided a desciption of an emergency situation. Your goal is to figure out that based on the situation given what emergency treatments and specialities would be in need andusesful to treat the situation. Make sure you response is a parsable json exactly like this  {
+  specialities: string[];
+  treatments: string[]; }
+`;
+
+export const userSituation = (situation: string): string => {
+  const description = `Ask the situation of the emergency if not already given; then, recommend First Aid if applicable to the situation and generate treatment from the list to figure out the possible treatmeant and return the json output. Be concise, and reduce the number of questions asked. Description of Situation: ${situation};`
+  return description;
+};
+// const description = `Someone is in an health emergency sitation and you will be given a desciption about the situation. do your best to generate the treatment from the list to figure out possible treatment and situation and return the json output. Description of Situation: ${situation}`;
+// Ask the situation of the emergency; then check if First Aid could help the user; if not, tell the user the nearest hospital that treat the patient Description of Situation: ${situation};

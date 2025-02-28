5ddf393063790276e8778836a66bef8fdcc92b23
unknown <emmanueladesipe01@gmail.com>
2025-02-28 01:37:12 +0100

    fixing typescript 4

--- vital-aids/next.config.ts
+++ vital-aids/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
+  remotePatterns: {
     domains: ["firebasestorage.googleapis.com"], // Replace with the actual domain
   },
 };

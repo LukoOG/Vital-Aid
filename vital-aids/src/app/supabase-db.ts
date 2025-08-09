c7bd91429bc9e57e1ca0468832c9bb7f0a191ea9
unknown <emmanueladesipe01@gmail.com>
2025-08-09 04:20:35 -0700

    supabase hot fix

--- vital-aids/src/app/supabase-db.ts
+++ vital-aids/src/app/supabase-db.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseurl: string = process.env.SUPABASE_URL || ""
-const supabasekey: string = process.env.SUPABASE_KEY || ""
+const supabaseurl: string = process.env.SUPABASE_URL || "https://vkxybcxpcbfejfltzaek.supabase.co"
+const supabasekey: string = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZreHliY3hwY2JmZWpmbHR6YWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3MzIzMjEsImV4cCI6MjA3MDMwODMyMX0.rrtajLD6TVoZ-B9AdlxzINpr0a4zce7hru4QmLtsP0Q"
 
 const supabase: SupabaseClient = createClient(supabaseurl, supabasekey)
 
